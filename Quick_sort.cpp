cb1203a094e94d44e0c5648a68993f12a0807467
umangmodii <Umangmodi003@gmail.com>
2024-01-30 12:42:38 +0530

    third commit

--- Quick_sort.cpp
+++ Quick_sort.cpp
@@ -2,66 +2,60 @@
 
 using namespace std;
 
-void base(int array[], int n)
+/* function that consider last element as pivot,
+place the pivot at its exact position, and place
+smaller elements to left of pivot and greater
+elements to right of pivot.  */
+
+int partition(int a[], int start, int end)
 {
-    for (int i = 0; i < n; i++)
+    int pivot = a[end]; // pivot element
+    int i = (start - 1);
+
+    for (int j = start; j <= end - 1; j++)
     {
-        cout << array[i] << " ";
+        // If current element is smaller than the pivot
+        if (a[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
     }
+    int t = a[i + 1];
+    a[i + 1] = a[end];
+    a[end] = t;
+    return (i + 1);
 }
 
-void quick_sort(int array[], int start, int end)
+/* function to implement quick sort */
+void quick(int a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */
 {
-
     if (start < end)
     {
-        int p = partition(array, start, end); // p is Partition sort
-        quick_sort(array, start, p - 1);           // left subarray in sort
-        quick_sort(array, p + 1, end);             // right subarray in sort
+        int p = partition(a, start, end); // p is the partitioning index
+        quick(a, start, p - 1);
+        quick(a, p + 1, end);
     }
 }
 
-int partition(int array[], int start, int end)
+/* function to print an array */
+void printArr(int a[], int n)
 {
-    int pivot = array[end];
-
-    for (int i = 0; i < end - 1; i++)
-    {
-        if (array[pivot] < array[end])
-        {
-            swap(array[i], array[i + 1]);
-        }
-
-        else if (array[pivot] > array[start])
-        {
-            array[start];
-        }
-
-        else
-        {
-            continue;
-        }
-    }
-    return 0;
+    int i;
+    for (i = 0; i < n; i++)
+        cout << a[i] << " ";
 }
-
 int main()
 {
-    int array[] = {24, 9, 29, 14, 19, 27};
-    // int start, end;
-
-    int n = sizeof(array) / sizeof(array[0]);
-
-    cout << "Before Elements are sorted - \n";
-
-    base(array, n);
-    quick_sort(array, 0, n - 1);
-
-    cout << "\n";
-
-    cout << "After Elements are sorted - \n";
-
-    base(array, n);
+    int a[] = {23, 8, 28, 13, 18, 26};
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << "Before sorting array elements are - \n";
+    printArr(a, n);
+    quick(a, 0, n - 1);
+    cout << "\nAfter sorting array elements are - \n";
+    printArr(a, n);
 
     return 0;
 }
