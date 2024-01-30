6294f1852fc52c6ae42b93bd89f9319a5ddf4d66
umangmodii <Umangmodi003@gmail.com>
2024-01-30 11:12:38 +0530

    change

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -14,7 +14,7 @@ void insertion_sort(int array[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++) // Check
         {
             if (array[i] > array[j])
             {
