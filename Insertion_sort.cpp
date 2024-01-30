2d7e007bc32b51668cda8eaa2246ddba932c1f41
umangmodii <Umangmodi003@gmail.com>
2024-01-30 11:02:50 +0530

    commit

---
+++ Insertion_sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+void base(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
+}
+
+void insertion_sort(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (array[i] > array[j])
+            {
+                array[i];
+            }
+
+            else
+            {               
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int array[] = {12, 31, 25, 8, 32, 17};
+
+    int n = sizeof(array) / sizeof(array[0]);
+
+    cout << "Before sorted array Elements are - \n";
+
+    base(array, n);
+    insertion_sort(array, n);
+
+    cout << "\n";
+
+    cout << "After sorted array Elements are - \n";
+
+    base(array, n);
+
+    return 0;
+}
